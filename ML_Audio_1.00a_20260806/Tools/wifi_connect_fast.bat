d43058648a4a66fc96f4c419ad15677ff0a0a297
fleamyth <fleamyth@gmail.com>
2026-09-01 17:37:26 +0800

    update wifi_connect_fast.bat to improve ADB Wi-Fi connection handling

--- ML_Audio_1.00a_20260806/Tools/wifi_connect_fast.bat
+++ ML_Audio_1.00a_20260806/Tools/wifi_connect_fast.bat
@@ -19,7 +19,7 @@ SET "STATUS="
 echo.
 echo ########## Android Wi-Fi ADB Setup (Optimized) ##########
 echo.
-
+:start
 echo [+] Resetting ADB server for a clean start...
 adb kill-server >nul
 REM The 'timeout /t 1' was removed as it's not needed. The next adb command will start the server if required.
@@ -120,6 +120,7 @@ if "%STATUS%"=="device" (
     echo ^| Could not verify a stable wireless connection.
     echo ^| Try running the script again.
     echo +--------------------------------------------------+
+    goto start
 )
 echo.
 
